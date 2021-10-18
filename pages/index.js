e43a6b8e36409d4636cdf5cb6c37b66a1b4b7f23
Shagai-555 <shagai555e@gmail.com>
2021-10-18 20:07:14 +0800

    zasalt

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,22 @@ import {
   Card,
   NavLink,
 } from 'react-bootstrap'
-import MyNavbar from "../components/my-navbar";
 export default function Home() {
   return (
     <Container>
-      <MyNavbar />
+      <Navbar className="fj-navbar fj-nav-base" bg="transparent" expand="lg">
+        <Navbar.Brand className="fj-navbar-brand">
+          <a href="#">1234 БЛОГ</a>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link className="fj-navbar-item fj-navbar-link" href="/">
+              НҮҮР
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <div className="blog-detail-page">
         <Row>
           <Col md="8">
